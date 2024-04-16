4ec307ee9ee1816c83559e2c5e8febc92bdfc5bf
Murat Simsek <muratsimseekk@gmail.com>
2024-04-16 16:48:42 +0300

    CategoryException and GlobalExceptionHandler created

---
+++ src/main/java/com/workintech/backend/exception/CategoryException.java
@@ -0,0 +1,16 @@
+package com.workintech.backend.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CategoryException extends RuntimeException{
+
+    private HttpStatus httpStatus;
+
+
+    public CategoryException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+}
