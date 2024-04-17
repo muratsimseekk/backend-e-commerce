7b3179a94744b830aa372fa25a336c3199874953
Murat Simsek <muratsimseekk@gmail.com>
2024-04-17 18:15:02 +0300

    Creates User entity

---
+++ src/main/java/com/workintech/backend/entity/User.java
@@ -0,0 +1,33 @@
+package com.workintech.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "user" , schema = "ecommercebackend")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+
+
+}
