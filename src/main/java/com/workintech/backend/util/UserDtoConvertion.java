986064917b4484717d46b2001de47654fcfa0221
Murat Simsek <muratsimseekk@gmail.com>
2024-04-17 19:44:17 +0300

    Adds UserResponse and UserDtoConvertion

---
+++ src/main/java/com/workintech/backend/util/UserDtoConvertion.java
@@ -0,0 +1,22 @@
+package com.workintech.backend.util;
+
+import com.workintech.backend.dto.UserResponse;
+import com.workintech.backend.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDtoConvertion {
+
+    public static List<UserResponse> convertUserList (List<User> userList){
+        List<UserResponse> userResponses = new ArrayList<>();
+
+        userList.stream().forEach(user -> userResponses.add(new UserResponse(user.getId(), user.getName(), user.getEmail())));
+
+        return userResponses;
+    }
+
+    public static UserResponse convertUser(User user) {
+        return new UserResponse(user.getId(), user.getName(), user.getEmail());
+    }
+}
