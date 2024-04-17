539af63a15b9178f32233b035ba3b2aaf0a8dc8c
Murat Simsek <muratsimseekk@gmail.com>
2024-04-17 17:41:44 +0300

    Creates StoreResponse and StoreDtoConvertion

---
+++ src/main/java/com/workintech/backend/util/StoreDtoConvertion.java
@@ -0,0 +1,25 @@
+package com.workintech.backend.util;
+
+import com.workintech.backend.dto.StoreResponse;
+import com.workintech.backend.entity.Store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StoreDtoConvertion {
+
+    public static List<StoreResponse> converStoreList (List<Store> stores){
+        List<StoreResponse> storeResponses = new ArrayList<>();
+
+        stores.stream().forEach(store -> storeResponses.add(new StoreResponse(store.getId(), store.getName()
+                , store.getPhone(), store.getTaxNo(), store.getBankAccount(), store.getProducts())));
+
+        return storeResponses;
+    }
+
+    public static StoreResponse converStore(Store store){
+        return new StoreResponse(store.getId(), store.getName(), store.getPhone()
+                , store.getTaxNo(), store.getBankAccount(), store.getProducts());
+    }
+
+}
