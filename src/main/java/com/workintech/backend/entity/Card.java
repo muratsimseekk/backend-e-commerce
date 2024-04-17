65c8fc9ff2299467fb4df252b8d31ed872c6274d
Murat Simsek <muratsimseekk@gmail.com>
2024-04-17 18:16:13 +0300

    Creates Card entity

--- src/main/java/com/workintech/backend/entity/Card.java
+++ src/main/java/com/workintech/backend/entity/Card.java
@@ -17,10 +17,18 @@ public class Card {
     @Column(name = "card_id")
     private Long cardId ;
 
-
+    @Column(name = "card_no")
     private Long cardNo;
+
+    @Column(name = "card_expire_month")
     private Long cardExpireMonth;
+
+    @Column(name = "card_expire_year")
     private Long cardExpireYear;
+
+    @Column(name = "card_ccv")
     private Long cardCcv;
+
+    @Column(name = "card_name")
     private Long cardName;
 }
